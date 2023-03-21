ed1db1ca3021666149449fd00e6c2f5e5384914a
ziad mohamed <77482848+ziaaddd@users.noreply.github.com>
2023-03-22 01:22:33 +0200

    Add files via upload

---
+++ js/natour.js
@@ -0,0 +1,116 @@
+// function wordBeforeLastLength(sentence) {
+
+//     myArr = sentence.split(" ");
+
+//     let newArr = myArr.filter((element) => element.length>=2);
+
+//     if(newArr.length == 1){
+//         return false
+//     }
+
+//     if(newArr.length == 2){
+//         return newArr[0]
+//     }
+
+//     if(newArr.length > 2){
+//         return newArr.slice(-2, - 1)
+//     }
+    
+
+
+// return newArr
+
+
+
+
+// }
+
+//   console.log(wordBeforeLastLength("Elzero Web School")); // Web => 3
+//   console.log(wordBeforeLastLength("Hello Elzero Web School    A ")); // Web => 3
+//   console.log(wordBeforeLastLength("Hello Elzero    Web   School  ")); // Web => 3
+//   console.log(wordBeforeLastLength("Hello")); // False
+//   console.log(wordBeforeLastLength("Hello Elzero")); // Hello
+//   console.log(wordBeforeLastLength("Hello Elzero    A")); // Hello
+
+// // 2
+// let myselect = document.querySelector(".select")
+// let myopt = document.querySelectorAll(".select option")
+
+// // console.log(myselect)
+// // console.log(myopt)
+
+// function eslector(){
+//     if(myselect.value === "sans"){
+//         console.log("font is sans")
+//     }
+// }
+
+// eslector()
+
+
+// 3
+// let zero = 0;
+// let counter = 3;
+// let my = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"];
+
+
+
+// console.log(my.slice(0, 4).reverse()); // ["Osama", "Elham", "Mazero", "Ahmed"];
+// console.log(my.slice(-5, -3).reverse()); // ["Elham", "Mazero"]
+// console.log(my[3]); // "Elzero"
+// console.log(my[1].slice(-2, -1)+my[1].slice(-1).toUpperCase()); // "rO"
+
+
+// // 11
+// function statuss(a, b, c){ 
+
+// if(typeof(a) === "string"){
+//     console.log(`hello ${a}, `)
+// }else if(typeof(a) === "number"){
+//     console.log(`age is  ${a}, `)
+
+// }else if(typeof(a) === "boolean"){
+//     if(a === true){
+//         console.log("Available For Hire")
+//     }else{
+//         console.log("not Available For Hire")
+//     }
+// }
+
+// if(typeof(b) === "string"){
+//     console.log(`hello ${b}, `)
+// }else if(typeof(b) === "number"){
+//     console.log(`age is  ${b}, `)
+
+// }else if(typeof(b) === "boolean"){
+//     if(b === true){
+//         console.log("Available For Hire")
+//     }else{
+//         console.log("not Available For Hire")
+//     }
+// }
+
+// if(typeof(c) === "string"){
+//     console.log(`hello ${c}, `)
+// }else if(typeof(c) === "number"){
+//     console.log(`age is  ${c}, `)
+
+// }else if(typeof(c) === "boolean"){
+//     if(c === true){
+//         console.log("Available For Hire")
+//     }else{
+//         console.log("not Available For Hire")
+//     }
+// }
+
+// }
+
+// statuss("Osama", 38, true) // "Hello Osama, Age Is 38, Available For Hire"
+// statuss(38, "Osama", true) // "Hello Osama, Age Is 38, Available For Hire"
+// statuss(false, 38, "Osama") // "Hello Osama, Age Is 38, Available For Hire"
+
+// console.log(mohamed.innerHTML+sayed.innerHTML)
+// console.log(+numMohamed.innerHTML+ +numSayed.innerHTML)
+
+
+
